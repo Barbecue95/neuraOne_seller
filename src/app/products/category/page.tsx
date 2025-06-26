40c131a6e00da38cf2e29c7d9e862f22088f5adc
phonenaykha-mhs <phonenaykha.jerry@gmail.com>
2025-06-26 18:04:55 +0630

    feat: Category UI

--- src/app/products/category/page.tsx
+++ src/app/products/category/page.tsx
@@ -1,9 +1,18 @@
-import { ComingSoon } from "@/components/comingsoon";
+"use client";
+import SubNavbar from "@/components/SubNavbar";
+import Table from "@/features/category/Table";
 
 export default function Home() {
   return (
     <div className="h-full">
-      <ComingSoon />
+      <SubNavbar
+        title="Product Category List"
+        buttons={[
+          { title: "Sort By", action: () => {} },
+          { title: "Add Product", action: () => {} },
+        ]}
+      />
+      <Table />
     </div>
   );
 }
